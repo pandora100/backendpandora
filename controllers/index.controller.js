3f54cb5847058b028cd282be6d6251344ab592d6
moncho01 <rramossh@hotmail.com>
2020-08-10 21:03:35 -0400

    PG1007 V1

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -28,7 +28,113 @@ indexCtrl.getUsers = (req, res) => {
    })
   };
 
+/*  
+indexCtrl.createUser = async (req, res) => {
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    res.json({
+        message: 'User Added successfully',
+        body: {
+            user: {name, email}
+        }
+    })
+};
+*/
+indexCtrl.createUser =(req, res) => {
+   const name = req.body.name;
+   const email = req.body.email;
 
+   pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email],(err) => {
+     if (err) {
+    return console.error('Error executing query', err.stack);
+    }
+                 res.json({
+                            message: 'User Added successfully',
+                            body: {
+                                user: {name, email}
+                              }
+              
+                        });
+     });//end pool.query
+};
+/*
+const getUserById = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const response = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    res.json(response.rows);
+};
+*/
+indexCtrl.getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
+    if (err) {
+    return console.error('Error executing query', err.stack);
+    }
+    
+    console.log('Bandera indexCtrl.getUserById end results:',result.rows);
+    return res.status(200).json(result.rows);
+    
+      }
+   
+  );//end pool.query
+};
+/*
+const updateUser = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, email } = req.body;
+
+    const response =await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+        name,
+        email,
+        id
+    ]);
+    res.json('User Updated Successfully');
+};
+*/
+indexCtrl.updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const name = req.body.name;
+    const email = req.body.email;
+
+    pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+        name,
+        email,
+        id
+    ], (err) => {
+         if (err) {
+         return console.error('Error executing query', err.stack);
+    }
+        
+        res.json('User Updated Successfully');
+        }
+
+    );//end pool.query
+   
+};
+
+/*
+const deleteUser = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query('DELETE FROM users where id = $1', [
+        id
+    ]);
+    res.json(`User ${id} deleted Successfully`);
+};
+*/
+indexCtrl.deleteUser =  (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query('DELETE FROM users where id = $1', [
+        id
+    ], (err) => {
+         if (err) {
+         return console.error('Error executing query', err.stack);
+    }
+
+    res.json(`User ${id} deleted Successfully`);
+     }
+    );//end pool.query
+    
+};
 
 
 module.exports = indexCtrl;
@@ -42,9 +148,10 @@ const pool = new Pool({
 };
 /// para heroku
 const pool = new Pool({
-   host: 'ec2-54-91-178-234.compute-1.amazonaws.com',
-   user: 'ygsstbshhyjhzd',
-   password: 'dc1341585983bd43fcb77fbad57c7ddac90cf54916b914fbeb0edbb44e52e86a',
-   database: 'd5b0rc9ekl9c02'
+   host: 'ec2-52-71-55-81.compute-1.amazonaws.com',
+   user: 'cblflfqkoftamy',
+   password: 'c3c022c436e2c3a11c3b4dc0c1e52433f4ddaa20ed2552afcd521784e951b87c',
+   database: 'd7pbr7epm2c2ml'
+
 };
 */
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 const indexController = require('../controllers/index.controller');
 
 // Routes
-router.get('/', indexController.getUsers);
 
+router.get('/users', indexController.getUsers);
+router.post('/users', indexController.createUser);
+router.get('/users/:id', indexController.getUserById);
+router.put('/users/:id', indexController.updateUser);
+router.delete('/users/:id', indexController.deleteUser);
 module.exports = router;
